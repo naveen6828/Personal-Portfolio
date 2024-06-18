f0afdd6b132c64fc8b8b8aa46ac8c4067991ddeb
naveen6828 <naveenkumar.jakkula98@gmail.com>
2024-06-18 15:42:47 -0400

    Implemented Skills section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import About from "./components/About"
 import Hero from "./components/Hero"
 import Navbar from "./components/navbar"
+import Technologies from "./components/Technologies"
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Navbar/>
         <Hero/>
         <About/>
+        <Technologies/>
       </div>
     </div>
   )
